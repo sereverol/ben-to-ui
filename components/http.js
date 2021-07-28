3e2602fb7a827da25c72ae9f27623b62c197d105
juan socorro <46764865+juansm17@users.noreply.github.com>
2021-07-28 16:20:31 -0400

    Add files via upload

---
+++ components/http.js
@@ -0,0 +1,30 @@
+//const url = 'https://pochita.herokuapp.com/'
+const urlx = 'http://192.168.1.132:3000/'
+const headers = { 'Content-Type': 'application/json' }
+
+
+const send = async (method, endpoint, body) => {
+    let response;
+
+    (body == null) 
+    ? response = await fetch(urlx + endpoint , { method, mode: 'cors', headers })
+    : response = await fetch(urlx + endpoint , { method, mode: 'cors', headers, body: JSON.stringify(body) });
+    
+    return await response.json(); 
+}
+
+const api = async (method, dir, body) => {
+    let response;
+
+    (body == null) 
+    ? response = await fetch(urlx + endpoint , { method, mode: 'cors', headers })
+    : response = await fetch(dir, { method, mode: 'cors', headers, body: JSON.stringify(body) });
+    
+    return await response.json(); 
+}
+
+
+export default {
+    send,
+    api
+}
